73746e2d7f0b43700753ac765b30f568b7bd217b
hakanaktan06 <alpaslanezik@gmail.com>
2026-03-11 14:43:28 +0300

    Update phone.js

--- js/shared/phone.js
+++ js/shared/phone.js
@@ -1,1 +1,23 @@
+export function normalizePhone(phone) {
 
+  let p = phone.replace(/\D/g, '');
+
+  if (p.startsWith("0")) {
+    p = p.substring(1);
+  }
+
+  if (!p.startsWith("90")) {
+    p = "90" + p;
+  }
+
+  return p;
+}
+
+export function displayPhone(phone){
+
+  if(phone.startsWith("90")){
+    phone = phone.substring(2);
+  }
+
+  return "0" + phone;
+}
