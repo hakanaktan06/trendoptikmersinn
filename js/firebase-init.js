cffac9d3220ac4204d58f1743aeb45bc3e7f3275
hakanaktan06 <alpaslanezik@gmail.com>
2026-03-11 15:00:57 +0300

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -3,12 +3,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKROzPt8ZaQXk_m5sMEY853BCafnZFb4o",
-  authDomain: "trendoptik.firebaseapp.com",
-  projectId: "trendoptik",
-  storageBucket: "trendoptik.firebasestorage.app",
-  messagingSenderId: "645619125341",
-  appId: "1:645619125341:web:a43aad6e213c80fbe351b4"
+apiKey:"AIzaSyDKROzPt8ZaQXk_m5sMEY853BCafnZFb4o",
+authDomain:"trendoptik.firebaseapp.com",
+projectId:"trendoptik",
+storageBucket:"trendoptik.firebasestorage.app",
+messagingSenderId:"645619125341",
+appId:"1:645619125341:web:a43aad6e213c80fbe351b4"
 };
 
 const app = initializeApp(firebaseConfig);
